52ac33b37e63df094798f2b11b778eba23b2adc9
Tushar Sonawane <tusharlsonawane@gmail.com>
2015-12-23 15:54:08 +0530

    updating angular.component type

--- angular-component/angular-component.d.ts
+++ angular-component/angular-component.d.ts
@@ -7,35 +7,39 @@
 
 declare module angular {
 
-  ///////////////////////////////////////////////////////////////////////////
-  // Module
-  // see http://docs.angularjs.org/api/angular.Module
-  ///////////////////////////////////////////////////////////////////////////
-  interface IModule {
-    /**
-     * Register a new component with the compiler.
-     *
-     * @param name Name of the component in camel-case (i.e. ngBind which will match as ng-bind)
-     * @param options Component definition object.
-     */
-    component(name: string, options: IComponent): IModule;
-  }
+    ///////////////////////////////////////////////////////////////////////////
+    // Module
+    // see http://docs.angularjs.org/api/angular.Module
+    ///////////////////////////////////////////////////////////////////////////
+    interface IModule {
+        /**
+         * Register a new component with the compiler.
+         *
+         * @param name Name of the component in camel-case (i.e. ngBind which will match as ng-bind)
+         * @param options Component definition object.
+         */
+        component(name: string, options: IComponentOptions): IModule;
+    }
 
-  ///////////////////////////////////////////////////////////////////////////
-  // Component
-  // see https://github.com/toddmotto/angular-component
-  ///////////////////////////////////////////////////////////////////////////
-  interface IComponent {
-    bindings?: Object;
-    controller?: string|Function;
-    controllerAs?: string;
-    isolate?: boolean;
-    template?: string|Function;
-    templateUrl?: string|Function;
-    transclude?: boolean;
-    restrict?: string;
-    $canActivate?: Function;
-    $routeConfig?: Object
-  }
+    ///////////////////////////////////////////////////////////////////////////
+    // Component
+    // see https://github.com/toddmotto/angular-component
+    ///////////////////////////////////////////////////////////////////////////
+    interface IComponentOptions {
+        bindings?: Object;
+        controller?: string | Function;
+        controllerAs?: string;
+        isolate?: boolean;
+        template?: string | IComponentTemplateFn;
+        templateUrl?: string | IComponentTemplateFn;
+        transclude?: boolean;
+        restrict?: string;
+        $canActivate?: Function;
+        $routeConfig?: Object;
+    }
+
+    interface IComponentTemplateFn {
+        ( $element?: IAugmentedJQuery, $attrs?: IAttributes ): string;
+    }
 
 }
